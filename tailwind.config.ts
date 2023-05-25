4a6ea6e2f7647112640758379af5c294e0c76a3b
brian-ngyn <brian.nguy1en@gmail.com>
2023-05-24 23:16:16 -0600

    blah

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "off-white": "#F6F2EC",
+        "green": "#4E7A51",
+        "black": "#121212",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
