d2c5012c2616ef48d3fb56a1dd3c02c253276fc9
brian-ngyn <brian.nguy1en@gmail.com>
2023-05-25 09:23:52 -0600

    [be] swap to supabase

--- src/pages/join-us/index.tsx
+++ src/pages/join-us/index.tsx
@@ -2,12 +2,12 @@ import { api } from "@/utils/api";
 import { type NextPage } from "next";
 
 const JoinUsPage: NextPage = () => {
-  const openExecPositions = api.openExecPosition.getAll.useQuery();
+  const { data } = api.openExecPosition.getAll.useQuery();
   return (
     <>
       <div className="items-center min-h-screen flex flex-col justify-center text-black bg-off-white">
         <div>Join Us Page</div>
-        {openExecPositions.data?.map((openExecPosition) => {
+        {data?.map((openExecPosition) => {
           return (
             <div key={openExecPosition.id}>
               <div>
