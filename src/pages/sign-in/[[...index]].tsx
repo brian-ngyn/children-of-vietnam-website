0a282406ccd388c7bec3587ba903e69b11000cf3
brian-ngyn <brian.nguy1en@gmail.com>
2023-05-25 09:38:36 -0600

    [fe] delete sign-up since restricting to google sign in

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,7 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
 
-const SignInPage = () => (
-  <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-);
+const SignInPage = () => {
+  return (
+    <>
+      <div className="items-center min-h-screen flex flex-col pt-[10%] text-black bg-off-white">
+        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      </div>
+    </>
+  );
+};
 
 export default SignInPage;
