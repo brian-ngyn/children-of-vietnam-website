fa48ead34e30a165c975445a17cc61a25a645608
brian-ngyn <brian.nguy1en@gmail.com>
2023-05-25 20:30:44 -0600

    [fe] added loader

---
+++ src/components/LoadingPage.tsx
@@ -0,0 +1,33 @@
+export const LoadingSpinner = (props: { size?: number }) => {
+  return (
+    <div role="status">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="m-auto bg-none block"
+        width="200px"
+        height="200px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <path d="M23 50A27 27 0 0 0 77 50A27 29.1 0 0 1 23 50" fill="#4A6FA5" stroke="none">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            dur="1s"
+            repeatCount="indefinite"
+            keyTimes="0;1"
+            values="0 50 51.05;360 50 51.05"
+          ></animateTransform>
+        </path>
+      </svg>
+    </div>
+  );
+};
+
+export const LoadingPage = () => {
+  return (
+    <div className="flex min-h-screen min-w-screen justify-center pt-64 bg-off-white">
+      <LoadingSpinner size={60} />
+    </div>
+  );
+};
