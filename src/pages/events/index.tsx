a400ceebf8c41575019972eee7a4eae8f2bb95b8
brian-ngyn <brian.nguy1en@gmail.com>
2023-05-25 20:35:31 -0600

    [fe] start events page

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,24 +1,29 @@
+import { LoadingPage } from "@/components/LoadingPage";
 import { api } from "@/utils/api";
 import { type NextPage } from "next";
 
 const EventsPage: NextPage = () => {
-  const { data } = api.event.getAll.useQuery();
+  const { data, isLoading } = api.event.getAll.useQuery();
 
   return (
     <>
-      <div className="items-center min-h-screen flex flex-col justify-center text-black bg-off-white">
-        <div>Events Page</div>
-        <div>
-          {data?.map((event) => {
-            console.log(event);
-            return (
-              <div key={event.id}>
-                <div>{event.eventTitle}</div>
-              </div>
-            );
-          })}
+      {isLoading ? (
+        <LoadingPage />
+      ) : (
+        <div className="items-center min-h-screen flex flex-col pt-64 text-black bg-off-white">
+          <div className="text-5xl font-bold">TODO</div>
+          <div>Events Page</div>
+          <div className="">
+            {data?.map((event) => {
+              return (
+                <div key={event.id}>
+                  <div className="break-all">{JSON.stringify(event)}</div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
