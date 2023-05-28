6b983dc4c174c72c59d47a6559ea815178eb784d
brian-ngyn <brian.nguy1en@gmail.com>
2023-05-27 22:37:11 -0600

    supabase image loader

---
+++ src/hooks/imgLoader.tsx
@@ -0,0 +1,13 @@
+const projectId = "tapwfxubfpcauhtwphkr";
+
+interface imageLoaderProps {
+  src: string;
+  width: number;
+  quality?: number;
+}
+
+export default function supabaseLoader({ src, width, quality }: imageLoaderProps) {
+  return `https://${projectId}.supabase.co/storage/v1/object/public/${src}?width=${width}&quality=${
+    quality || 75
+  }`;
+}
