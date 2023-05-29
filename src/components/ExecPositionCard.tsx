61b5d1f96db0654d4cf84387df52abe8aea18fb9
brian-ngyn <brian.nguy1en@gmail.com>
2023-05-28 22:43:00 -0600

    [fe] open exec position card component

---
+++ src/components/ExecPositionCard.tsx
@@ -0,0 +1,43 @@
+import { type OpenExecPosition } from "@prisma/client";
+import Link from "next/link";
+
+interface OpenExecPositionCardProps {
+  openExecPosition: OpenExecPosition;
+}
+
+const OpenExecPositionCard = ({ openExecPosition }: OpenExecPositionCardProps) => {
+  const { id, positionTitle, positionDescription, positionLink } = openExecPosition;
+
+  return (
+    <>
+      <div className="flex items-stretch flex-col w-full h-full rounded-lg border bg-gray-100 p-4 shadow-lg transition hover:shadow-xl sm:p-6 shadow-black-200">
+        <div className="grow">
+          <div>
+            <div className="mt-0.5 text-lg font-medium text-black">{positionTitle}</div>
+          </div>
+
+          <div className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+            {positionDescription}
+          </div>
+        </div>
+
+        <div className="self-start">
+          <Link
+            href={positionLink ? positionLink : "https://www.instagram.com/thechildrenofvietnam/"}
+            className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600"
+          >
+            Apply
+            <span
+              aria-hidden="true"
+              className="block transition-all group-hover:ms-0.5 rtl:rotate-180"
+            >
+              &rarr;
+            </span>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OpenExecPositionCard;
