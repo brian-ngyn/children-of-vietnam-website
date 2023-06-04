b0bb765202345d1124227df10628a595aa579764
brian-ngyn <brian.nguy1en@gmail.com>
2023-06-03 22:20:50 -0600

    [be] get all users route

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -37,4 +37,24 @@ export const adminRouter = createTRPCRouter({
         };
       }
     }),
+
+  getAllUsers: privateProcedure.query(async ({ ctx }) => {
+    const userSubmittingRequest = await users.getUser(ctx.userId);
+    if (!userSubmittingRequest.publicMetadata.is_admin_verified) {
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: "You are not authorized to view all users",
+      });
+    }
+
+    const allUsers = await users.getUserList();
+    if (!allUsers) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to get all users",
+      });
+    } else {
+      return allUsers;
+    }
+  }),
 });
