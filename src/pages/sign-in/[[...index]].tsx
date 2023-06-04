27bb6a86295fea8ebe41d841d189d74f68a880a5
brian-ngyn <brian.nguy1en@gmail.com>
2023-06-03 22:34:14 -0600

    [fe] login logic

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,12 +1,29 @@
+import { LoadingPage } from "@/components/LoadingPage";
 import { SignIn } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const SignInPage: NextPage = () => {
+  const { isLoaded, isSignedIn } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded && isSignedIn) {
+      void router.push("/admin");
+    }
+  }, [isLoaded, isSignedIn, router]);
+
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center bg-off-white pt-[10%] text-black">
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" redirectUrl="/admin" />
-      </div>
+      {isLoaded && !isSignedIn ? (
+        <div className="flex min-h-screen flex-col items-center bg-off-white pt-[10%] text-black">
+          <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" redirectUrl="/admin" />
+        </div>
+      ) : (
+        <LoadingPage />
+      )}
     </>
   );
 };
--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,12 +1,29 @@
+import { LoadingPage } from "@/components/LoadingPage";
 import { SignUp } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const SignUpPage: NextPage = () => {
+  const { isLoaded, isSignedIn } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded && isSignedIn) {
+      void router.push("/admin");
+    }
+  }, [isLoaded, isSignedIn, router]);
+
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center bg-off-white pt-[10%] text-black">
-        <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" redirectUrl="/admin" />
-      </div>
+      {isLoaded && !isSignedIn ? (
+        <div className="flex min-h-screen flex-col items-center bg-off-white pt-[10%] text-black">
+          <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" redirectUrl="/admin" />
+        </div>
+      ) : (
+        <LoadingPage />
+      )}
     </>
   );
 };
