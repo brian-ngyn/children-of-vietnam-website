2dee4d864e30f50469981760aaa27dfa9746b191
brian-ngyn <brian.nguy1en@gmail.com>
2023-08-17 19:41:22 -0600

    [be] update, delete and edit team members

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -4,6 +4,7 @@ import { users } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
 
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
+import { prisma } from "~/server/db";
 
 export const adminRouter = createTRPCRouter({
   verifyUser: privateProcedure
@@ -59,4 +60,121 @@ export const adminRouter = createTRPCRouter({
       return allUsers;
     }
   }),
+
+  updateTeamMember: privateProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        memberName: z.string(),
+        memberRole: z.string(),
+        memberDescription: z.string(),
+        // memberImage: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userSubmittingRequest = await users.getUser(ctx.userId);
+      if (!userSubmittingRequest.publicMetadata.is_admin_verified) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "You are not authorized to update team members",
+        });
+      }
+
+      if (input) {
+        const updateTeamMember = await prisma.teamMember.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            memberName: input.memberName,
+            memberRole: input.memberRole,
+            memberDescription: input.memberDescription,
+            // memberImage: input.memberImage,
+          },
+        });
+        if (!updateTeamMember) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Failed to verify user",
+          });
+        } else {
+          return {
+            success: true,
+            message: `Updated team member ${input.id}`,
+          };
+        }
+      }
+    }),
+
+  newTeamMember: privateProcedure
+    .input(
+      z.object({
+        memberName: z.string(),
+        memberRole: z.string(),
+        memberDescription: z.string(),
+        // memberImage: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userSubmittingRequest = await users.getUser(ctx.userId);
+      if (!userSubmittingRequest.publicMetadata.is_admin_verified) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "You are not authorized to create users",
+        });
+      }
+
+      if (input) {
+        const newTeamMember = await prisma.teamMember.create({
+          data: {
+            memberName: input.memberName,
+            memberRole: input.memberRole,
+            memberDescription: input.memberDescription,
+            // memberImage: input.memberImage,
+          },
+        });
+        if (!newTeamMember) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Failed to verify user",
+          });
+        } else {
+          return {
+            success: true,
+            message: `Created new team member ${newTeamMember.id}`,
+          };
+        }
+      }
+    }),
+
+  deleteTeamMember: privateProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      const userSubmittingRequest = await users.getUser(ctx.userId);
+      if (!userSubmittingRequest.publicMetadata.is_admin_verified) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "You are not authorized to delete team members",
+        });
+      }
+
+      if (input) {
+        const deleteTeamMember = await prisma.teamMember.delete({
+          where: {
+            id: input,
+          },
+        });
+        if (!deleteTeamMember) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Failed to verify user",
+          });
+        } else {
+          return {
+            success: true,
+            message: `Deleted team member ${input}`,
+          };
+        }
+      }
+    }),
 });
